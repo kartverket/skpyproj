da8437c843fa2f9f692285fcbd3f2a44113c9d6d
Sveinung Himle <Sveinung.Himle@kartverket.no>
2021-09-17 11:21:29 +0200

    Prototype works

--- src/proj_pointcloud.py
+++ src/proj_pointcloud.py
@@ -1,46 +1,48 @@
 
 import numpy as np
-import csv as csvinput 
+import csv as csvinput
+import csv as csvoutput
 import argparse
 
-from pyproj import CRS
 from pyproj import Transformer, transform
 from pyproj.aoi import AreaOfInterest, AreaOfUse, BBox
 from pyproj.database import query_utm_crs_info, query_crs_info
+  
+parser = argparse.ArgumentParser(description='Transforms coordinates from csv files at format (ID x y z epoch).')
 
-##parser = argparse.ArgumentParser(description='Process some integers.')
-##parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
-##parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum, default=max, help='sum the integers (default: find the max)')
-
-parser = argparse.ArgumentParser(description='Transforms coordinates from csv files at format (ID,x,y,z,epoch).')
-
-parser.add_argument('input', metavar='InputFile', type=ascii, help='Path to input csv file')
-parser.add_argument('output', metavar='OutputFile', type=ascii, help='Path to output csv file')
+parser.add_argument('input', metavar='InputFile', type=str, help='Path to input csv file')
+parser.add_argument('output', metavar='OutputFile', type=str, help='Path to output csv file')
 parser.add_argument('epsgsource', metavar='EPSGSource', type=int, help='EPSG code source crs')
 parser.add_argument('epsgtarget', metavar='EPSGTarget', type=int, help='EPSG code target crs')
 
 args = parser.parse_args()
 print('args is: ', args)
 
-inFile = args.input
-outFile = args.output
+inFileName = args.input
+outFileName = args.output
 epsgsource = args.epsgsource
 epsgtarget = args.epsgtarget
 
-##print(args.accumulate(args.integers))
-
-#TRYS
-x_coords = 2987993.64255 # np.random.randint(80000, 120000)
-y_coords = 655946.42161  # np.random.randint(200000, 250000)
-z_coords = 5578690.43270 # np.random.randint(200000, 250000)
-epoch = 2020.00
-
 transformer1 = Transformer.from_crs(epsgsource, epsgtarget)
-res1 = transformer1.transform(x_coords, y_coords, z_coords, epoch)
-print(res1)
 
-crs = CRS.from_epsg(7789)
-crs = CRS.from_string("epsg:4326")
+outputFile = open(outFileName, "w")
 
-crs.to_epsg()
-crs.to_authority()
+with open(inFileName) as csvfile:
+    spamreader = csvinput.reader(csvfile, skipinitialspace = True, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        id = row[0]
+        x_coords = float(row[1])
+        y_coords = float(row[2])
+        z_coords = float(row[3])
+        epoch = float(row[4])
+        res = transformer1.transform(x_coords, y_coords, z_coords, epoch)
+        x_coords = res[0]
+        y_coords = res[1]
+        z_coords = res[2]
+        L = [id, ' ', str(x_coords), ' ', str(y_coords), ' ', str(z_coords), ' ', str(epoch)]      
+        outputFile.writelines(L)
+        outputFile.writelines('\n')
+        print(L)
+        print(row)
+
+outputFile.close()
