f43859678b7239bade951a0c7831a276d29200c6
Sveinung Himle <Sveinung.Himle@kartverket.no>
2021-09-21 09:56:00 +0200

    test works

--- src/testpyproj.py
+++ src/testpyproj.py
@@ -1,8 +1,13 @@
 import numpy as np
-from pyproj import CRS
+import sqlite3
+from pyproj import CRS, Proj
 from pyproj import Transformer, transform
 from pyproj.aoi import AreaOfInterest, AreaOfUse, BBox
 from pyproj.database import query_utm_crs_info, query_crs_info
+from pyproj.enums import PJType
+from pyproj.datadir import get_data_dir
+
+print(get_data_dir() + '\proj.db')
 
 fromepsgcode = 7789 #int(input("Enter EPSG code source crs: "))
 print(fromepsgcode)
@@ -13,25 +18,28 @@ print(toepsgcode)
 areaepsgcode = 1352 #int( input("Enter EPSG code area extent: "))
 print(areaepsgcode)
 
-# Norway 1352
-if areaepsgcode == 1352:
-    area = AreaOfInterest(4.68, 57.93, 31.22, 71.21)
-    aou = AreaOfUse(4.68, 57.93, 31.22, 71.21, "Norway - onshore")
-else:
-    area = AreaOfInterest(10.03, 54.96, 24.17, 69.07)
-    aou = AreaOfUse(10.03, 54.96, 24.17, 69.07, "Sweden")
+south_lat = north_lat = west_lon = east_lon = 0
 
-utm_crs_list = query_utm_crs_info(
-    datum_name="WGS 84",
-    area_of_interest=AreaOfInterest(
-        west_lon_degree=-93.581543,
-        south_lat_degree=42.032974,
-        east_lon_degree=-93.581543,
-        north_lat_degree=42.032974,
-    ),
-)
-utm_crs = CRS.from_epsg(utm_crs_list[0].code)
-print(utm_crs)
+#TODO: Move to new class
+connection = sqlite3.connect(get_data_dir() + '\proj.db')
+cursor = connection.cursor()
+cursor.execute("SELECT south_lat, north_lat, west_lon, east_lon FROM extent WHERE code = " + str(areaepsgcode) + ";")
+results = cursor.fetchall()
+if len(results) == 1:
+    south_lat = float(results[0][0])
+    north_lat = float(results[0][1])
+    west_lon = float(results[0][2])
+    east_lon = float(results[0][3])
+cursor.close()
+connection.close()
+
+area = AreaOfInterest(west_lon, south_lat, east_lon, north_lat)
+print(area)
+
+if area.east_lon_degree == 0 and area.north_lat_degree == 0 and area.south_lat_degree and area.west_lon_degree:
+    transformer = Transformer.from_crs(fromepsgcode, toepsgcode)
+else:
+    transformer = Transformer.from_crs(fromepsgcode, toepsgcode, area_of_interest = area)
 
 #TRYS
 x_coords = 2987993.64255 # np.random.randint(80000, 120000)
@@ -39,25 +47,14 @@ y_coords = 655946.42161  # np.random.randint(200000, 250000)
 z_coords = 5578690.43270 # np.random.randint(200000, 250000)
 epoch = 2020.00
 
-ttt = CRS.from_epsg(7789)
+res = transformer.transform(x_coords, y_coords, z_coords, epoch)
+print(res)
 
-transformer1 = Transformer.from_crs(fromepsgcode, toepsgcode, area_of_interest = area)
-#transformer1 = Transformer.from_crs(CRS.from_epsg(7789), CRS.from_epsg(4936))
-#transformer1 = Transformer.from_crs(CRS.from_epsg(7789), CRS.from_epsg(4936), area_of_interest = area)
-res1 = transformer1.transform(x_coords, y_coords, z_coords, epoch)
-print(res1)
-
-# Sweden 4378
-transformer2 = Transformer.from_proj(fromepsgcode, toepsgcode)
-res2 = transformer2.transform(x_coords, y_coords, z_coords, epoch)
-print(res2)
-
-transform(fromepsgcode, toepsgcode, x_coords, y_coords, z_coords, epoch)
-
+#test 
 crs = CRS.from_epsg(7789)
+print(crs)
 crs = CRS.from_string("epsg:4326")
+print(crs)
 
-crs.to_epsg()
-crs.to_authority()
-
-print(x_coords, y_coords, z_coords, epoch)
+print(crs.to_epsg())
+print(crs.to_authority())
